ae4e2723207f9688228c2db6f48e5d0a53b69344
Evan <evan.machefer@ac-dijon.fr>
2023-01-27 13:07:59 +0100

    [FEATURE] Ajout TP pointage

---
+++ physique/mouvement/pointage/TP_pointage.py
@@ -0,0 +1,52 @@
+##################################################
+########## NE PAS TOUCHER (voir ligne 40) ########
+##################################################
+from matplotlib import pyplot as plt
+
+def tracer(x, y, titre = "Titre du graphique"):
+   """
+   Fonction à appeler pour tracer le graphique.
+   Exemple pour tracer y = f(x) :
+   tracer(x, y)
+   """
+   plt.plot(x, y, 'bo')
+   plt.title(titre)
+   plt.ylabel("Altitude y (en m)")
+   plt.xlabel("Temps t (en s)")
+
+
+def vitesse(x, y):
+   """
+   Fonction permettant de calculer les vitesses et de les tracer
+   sur le graphique.
+   Exemple d'appel :
+   vitesse(x,y)
+   """
+   vy = []
+   vx = []
+   for i in range(len(y) - 1):
+      ### TODO : Ajouter la formule correspondante
+      vy_i = 0
+      ### TODO : Ajouter la formule correspondante
+      vx_i = 0
+      vy.append(vy_i)
+      vx.append(vx_i)
+
+   for i in range (len(vy)):
+      plt.arrow(x[i], y[i], vx[i]/20., vy[i]/20., length_includes_head=True, head_width = 0.01, head_length = 0.03)
+
+##################################################
+############# TRAVAIL A FAIRE ICI ################
+##################################################
+
+### TODO : coller le résultat du pointage t, x et y
+
+### TODO : appeler la fonction pour tracer y en fonction de t
+
+### TODO : modifier la fonction vitesse au niveau des TODO
+
+### TODO : appeler la fonction pour tracer les vitesses
+
+
+########### LAISSER EN FIN DE SCRIPT ################
+plt.show()
